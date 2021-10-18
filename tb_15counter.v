798b9e9ca167cdbe4f3588ef4ddcb4a98a2f63fc
Gyeongchae Na <88664915+gc-na@users.noreply.github.com>
2021-10-18 15:39:47 +0900

    Create tb_15counter.v

---
+++ tb_15counter.v
@@ -0,0 +1,16 @@
+module tb_mod15cnt ();
+reg clk=1'b0;
+reg nclr=1'b0;
+wire [3:0] q;
+
+  mod15cnt aaa(clk, nclr, q);
+always #5 clk = ~clk;
+initial begin
+$dumpfile("dump.vcd");
+$dumpvars;
+nclr=0;
+#20;
+nclr=1;
+#300 $finish;
+end
+endmodule
